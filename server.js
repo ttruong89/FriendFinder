f4efcb1b3c7e937bdfc0a6b33fe17d6df29f31ed
ttruong89 <t.truong2009@gmail.com>
2017-02-12 17:19:42 -0600

    fixed a bug in server.js

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ require("./app/routing/htmlRoutes")(app);
 
 // LISTENER
 // Message to show app listening on PORT...
-app.listen(provess.env.PORT || 5000, function() {
+app.listen(process.env.PORT || 5000, function() {
 	console.log("App listening on PORT: " + PORT);
 });
