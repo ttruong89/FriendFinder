85a1e53ad116ee172e214bb243d13d03ffac7144
ttruong89 <t.truong2009@gmail.com>
2017-02-12 11:59:33 -0600

    changed to image links instead

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // Friend array of objects.
 	var friends = [{
 		"name": "Kim",
-		"photo": "../app/public/assets/images/Kim.jpg",
+		"photo": "https://lh3.googleusercontent.com/0_PoA6FFlAJ9GHfPKPkQ-rlLeogfuhRxEagX6CsEqjFmLPig-YmG378JNAAv1wUVRFI0dRvfp1sQ0Ersd9dByMBxK5qIyZFRwQyrMPUhLPtbIUh5Aik-bLaAlpkaTHqlodFrb99EHS2nKL6HigfXWycfJqi4cBTp1SZMi6EpGXtNqS5iNcZRyf-sDvvsrBnMronId2ZvJ_qvchtYhZLdOyic5OVDNb04IBx6_dACiimZJm4DHkqorQupQwEH-gzsDI-j_fPcUAfsoC62CKT-7YnDdAAcw26_MAA0DaFG3DYZxXXFqrGb-NwYSU-fzuZBcqMXPAMoqezUhf7MDMePbqS7lm1zKgZ746gff6CGWCISur1s5hFpAzo-VaI6DCE9grhAUmoFAC18AixRZ4-SWGAAIKjtnWGdBmqkPb0gJ4RYwa7AoDiALVlFFYEPm54cFO0x2ezwcQw7yelPg7GtVl7C88IrKbQPgYIJD8ZQa9rb9Kdj9M-K1LA10e35E8XWB-WtPs3vCV2xrDRDoxS05axKoyt64DtH5N1VUnJqMcEdJ6zRnfSK5BJUgFR55aFjqo0nVTGPpAM2jbzt9aDMLfVybJz6NlAaaIlsiV_80kONzmpz=w1200-h630-no",
 		"scores": [
 			5, 
 			2, 
@@ -16,7 +16,7 @@
 		  ]
 	},	{
 		"name": "Rihanna",
-		"photo": "../app/public/assets/images/Rihanna.jpg",
+		"photo": "https://lh3.googleusercontent.com/nM-GKwoNtyQS4KEusdX8RkUZAfPwqLrmIaIuZAqL4HHp2icI_TshevFq6pnAkpH4tQUxHsaph3BlZmlOWdQONp7klocxb1CC3pHMhwEBSh1XS589JgkPWUMNaDwZK6i1RTdrM99wZyTcQYO-ptL-H-m1-rnpsBw42MB4Sj5YFTVwJH1m1-zKGK8KSFt_G1-MpUKyKwfRLdgWPIYVkCVy0GFDaYJbaWH-mF4qJL_ZTTUNkmk13DjzFN2YI3lLm8eD2HBAhDWplK1AlhJ4k6imsPgj9fXinujQHDzT-AKcnfo1F-hdU95t7EF0V4U9o47Mzarc_vKxrWX-S3uWpApKs0qy1uUXLWU0cYB2YN_Ct4Mm8rwYaw-iJUjEZ0q0c0FW5NeIElvVKZKKMFJO6TsbT8Cie5HuJHEdQIYMMCUlZxsNfzsQxxaQ9EesbQVnDlxsmE9c0CT9plrfJ-B6BCeikz3PSCiXZ1HhhjaLKlw62vBKFDyK1PAXK71YKERTXjIXrNcQsdygIX6zZi2RlCm_vICM9RYSPIf0byVx2hFXr--g-aLNQCBYg3lK2tElX0r_THq8CmpSD7H4vdoETJ66bwPYmdfvqCdRS9x6kW4hfOhkso8F6tuK=s529-no",
 		"scores": [
 			4, 
 			4,
@@ -31,7 +31,7 @@
 		  ]
 	},	{
 		"name": "Lebron",
-		"photo": "../app/public/assets/images/Lebron.jpg",
+		"photo": "https://lh3.googleusercontent.com/Sy4gip5-QxFJC25dkokEqenXvHRGI8XxlJAC2HYrLgAVdD1eozFNu86UjIc8rxgaZ2ijB8QQPCVkdcvTBprhtB1yyNyNChaQWmr7_exEZii-x1fXmYxhajFlb2KDQyIHNHjIUJrmTm7c_MrYIIMjOwNPQXw_3ejWKFRbYU836pjP4lkbDLSybbhNpdVF-x42vAOuQtE4G8cSK-zgiYP02Y0C9eFeaS3PvDwVSbty6HQDOnYxAeM4sWQhGk7fs_89qoM-DxH7nsnpF2hJzF11mybnA_ZvV0taQ6eK-FWal7S4kknsbeCi1a5R1LzOcj9JArrXAJurBSnKwjutnPIbP6TqD-JfU0iqkSe5bu6R17yjkoQEW1rAfIej2T7Gm9DR9K-Kr9TokcSIQrHAeq82i5803T1U__5OWHpw3aDqkF3heP8F1TU8V1qF3qGwAgklais_GgL3IEa087Lp7CA8QR0uMbG11ahojaGP4lb4VnHuDnQbH-f-1A9Lk6hX_TeOSWzr1sOK_JkFjyFONYWTXQMSWyFJ53derM5Ynf-46YsFuFJdQukjcMBmKVEiv7SO8szA00-Ajqd2spO8qBCwmMnnKsvF0bx_Mylu49XdR3IwHTHC=w360-h480-no",
 		"scores": [
 			4, 
 			1,
@@ -46,7 +46,7 @@
 		  ]
 	},	{
 		"name": "Dean",
-		"photo": "../app/public/assets/images/Dean.png",
+		"photo": "https://lh3.googleusercontent.com/y_86-CEV8vwr9RryYNdvLepnkpqIL6vZ-pSw80_bAdgHW3XMXpMXREFO2xzEyYKGawN1VurpmaOHNr-x2yameXcBdXRCN66TKnUdklwptQCoNVjgxxhHe2aemcGgsabxbCxZgjGO2virCZnH7D0XRdgUVmcT_X5CXiZnV-a6wNTCTLyri9puuecNjLdeFyRSorRwj_79lHHzpYBFbk4oWKLZGl3pDVnN0MZAV2IJ0QEuYhyj0A5EwwkXTboYvjKPgYbED4YX3hytQwfaMqw1h35K5qe_2f8SihfYgax3l_Ii111XcZ27l26zFXH-kJwZR8vP8nz62pMxQaAI15jrhQ0p0gZb3wFhJE-1uBp9r8AmyfSmLuFT67rnNwn4hLc8V4YXbBLxhlywZKZZBG2QxSwh6VkgkyzlqlB4J_bk4JJ3tBbe36j6hZiP_5gFYI0lGfDfcqsqzlf2aKmm7UzRZS07e9jsHaW-5Gtb5KMPy5bUksVjCbtP07rsbQ3x2a9-6N5u1J0Ru6pdfzMviPrvopkoG8y_uyvTPr7LzOwjGNoWKV3J2wKYlO8DpSIFz2qXmgogMeRz0JFSu1Y8SqFKrwD5vg3BC_pE412L78gB1dI7zC6o=w636-h421-no",
 		"scores": [
 			5, 
 			2,
@@ -61,7 +61,7 @@
 		  ]
 	},	{
 		"name": "Oprah",
-		"photo": "../app/public/assets/images/Oprah.jpg",
+		"photo": "https://lh3.googleusercontent.com/EeN0VsaP_YgbDjJ1w3X_Q5gCQovdQq7XadF7JJokxyKz07zeCfTtT-DD1wUvqsPsjxIN1otb5TikFk-fKpq2bTu3Gcj8TOGd2bpFEtaO5PTcQneiRg5cRH_RiHDdP2FFks7G7WJO1lNyQh0BP-FLbmi2ftXw9BhwpGe9BGz6tXStj_sHgJfDxeTfgMjckvgtba8apD52Iw_9UtC1gmyP06C3taLYawpvd_uw4Drh-N-yUdDqeCrvXOh7s5Zo1ZruhzYKpFiYPf4n0s2XjtXHJbXbznOt2-2v2f8P7LgTGMNe6adXK7Q-1lvgv8IevIuffw7IOgF7_LdVCrSR1zIKt_0Y64xHrUezujVIBntybG-0veBolex05kmRNp0RvL-rkk5Y0nnqvN2LX6uUqaTq1bmwx1QR6YK_nsNCGUihVuwO2TZZyY72U0-uahLJZWl6ePtcuUwWeaOv_iJMn4qeFvjX7RI-1m1XB362RY3p3EpyqzObo32m88tEzgC5FMFnmuvJGRcUfG9zEWuLGJYFRCpuPbByLjRU5BE3m7UjLvR_TNPhnHWm7Do42V5__T0YLSQrXHhnVCQ5QR4d_sRUQt2zlIZszE6cSnIR5m4fKWBiOT7M4_6J=w240-h320-no",
 		"scores": [
 			1, 
 			2,
