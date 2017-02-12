1a2600a0f68ded2254488369a3fcfe86c0af3b49
ttruong89 <t.truong2009@gmail.com>
2017-02-12 11:51:55 -0600

    adding some images and friends array of objects.

---
+++ app/data/friends.js
@@ -0,0 +1,80 @@
+// Friend array of objects.
+	var friends = [{
+		"name": "Kim",
+		"photo": "../app/public/assets/images/Kim.jpg",
+		"scores": [
+			5, 
+			2, 
+			3,
+			1,
+			4,
+			5,
+			3,
+			1,
+			2,
+			4,
+		  ]
+	},	{
+		"name": "Rihanna",
+		"photo": "../app/public/assets/images/Rihanna.jpg",
+		"scores": [
+			4, 
+			4,
+			5,
+			3,
+			2,
+			4,
+			5,
+			2,
+			1,
+			4,
+		  ]
+	},	{
+		"name": "Lebron",
+		"photo": "../app/public/assets/images/Lebron.jpg",
+		"scores": [
+			4, 
+			1,
+			3,
+			3,
+			2,
+			4,
+			2,
+			2,
+			1,
+			4,
+		  ]
+	},	{
+		"name": "Dean",
+		"photo": "../app/public/assets/images/Dean.png",
+		"scores": [
+			5, 
+			2,
+			5,
+			3,
+			4,
+			4,
+			1,
+			2,
+			1,
+			5,
+		  ]
+	},	{
+		"name": "Oprah",
+		"photo": "../app/public/assets/images/Oprah.jpg",
+		"scores": [
+			1, 
+			2,
+			3,
+			3,
+			2,
+			4,
+			5,
+			3,
+			1,
+			4,
+		  ]
+	}]; 
+
+// Export Array to make it accessible to files that require it.
+module.exports = friends;
